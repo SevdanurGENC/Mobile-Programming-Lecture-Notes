02713569a873a08cf46bb900b81a7f09503f8730
Sevdanur GENC <sevdanurgenc@gmail.com>
2021-03-18 15:18:28 +0300

    Create ListYapisi.dart

---
+++ DartDili/ListYapisi.dart
@@ -0,0 +1,51 @@
+void main() { 
+  
+  List isimler = ["Sevdanur", "Huseyin", "Zeynep", "Dogus", "Selcuk"];
+  
+  List yeniIsimler = ["Hakan", "Hayrettin", "Sahin"];
+  
+  //print(isimler);  
+  //print(isimler[2]);  
+  //print(isimler.length);  
+  //print(isimler.reversed); 
+  //print(isimler.first);
+  //print(isimler.last);
+  //print(isimler.isEmpty);
+  //print(isimler.isNotEmpty);
+  //print(isimler.runtimeType); 
+  //isimler.add("Hakan");
+  //print(isimler); 
+  //isimler.addAll(yeniIsimler);
+  //print(isimler); 
+  //print(isimler.asMap()); 
+  //isimler.clear();
+  //print(isimler); 
+  //isimler.fillRange(1,3);
+  //print(isimler); 
+  //print(isimler.getRange(1,3)); 
+  //print(isimler.indexOf("Dogus"));
+  //isimler.insert(2, "Hakan");
+  //print(isimler);
+  //isimler.insertAll(2, yeniIsimler);
+  //print(isimler);
+   
+  //print(isimler.lastIndexOf("Huseyin", 1));
+  //isimler.remove("Dogus");
+  //print(isimler);
+  //isimler.removeAt(3);
+  //print(isimler);
+  //isimler.removeLast();
+  //print(isimler);
+  //isimler.removeRange(1,3);
+  //print(isimler);
+  //isimler.replaceRange(1,3,yeniIsimler);
+  //print(isimler);
+  //isimler.setAll(0, yeniIsimler);
+  //print(isimler);
+  //isimler.setRange(1,3,["Hakan", "Hayrettin"]);
+  //print(isimler);
+  //isimler.sort();
+  //print(isimler); 
+  //print(isimler.sublist(2));
+  
+}
